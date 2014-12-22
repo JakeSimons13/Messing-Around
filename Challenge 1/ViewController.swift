c30f6d8b4c7fc926b926cefa8501320fa256fc9c
Jake Simons <jakesimons@Jakes-MacBook-Pro-2.local>
2014-12-21 22:50:42 -0600

    Trophy To James Postcard

--- Challenge 1/ViewController.swift
+++ Challenge 1/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var enterName: UITextField!
+    @IBOutlet weak var enterMessage: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendPostcardButtonPressed(sender: UIButton) {
+        
+        nameLabel.hidden = false
+        messageLabel.hidden = false
+        nameLabel.text = enterName.text
+        messageLabel.text = enterMessage.text
+        nameLabel.textColor = UIColor.redColor()
+        messageLabel.textColor = UIColor.redColor()
+        enterName.text = ""
+        enterMessage.text = ""
+        sendButton.setTitle("Welcome to Trophy you Son of a Bitch", forState: UIControlState.Normal)
+    }
 
 }
+
+
+
+
+
+
+
 
